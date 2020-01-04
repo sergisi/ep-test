89fabd00d5db300258b786baae80bc41b3d27336
Horno <i.palacin.a@gmail.com>
2020-01-04 18:10:01 +0100

    ProductSpecification test

---
+++ test/pharmacy/ProductSpecificationTest.java
@@ -0,0 +1,25 @@
+package pharmacy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductSpecificationTest {
+
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void setPrice() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getUpccode() {
+    }
+}
